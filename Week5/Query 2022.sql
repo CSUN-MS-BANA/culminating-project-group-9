ee333e918375d0f570c708939eb9ced9473c415e
Ida Karimi <106859477+IdaBas@users.noreply.github.com>
2025-03-08 22:56:54 -0800

    Add files via upload

---
+++ Week5/Query 2022.sql
@@ -0,0 +1,35 @@
+SELECT
+    c.CountryID, c.CountryName, c.Region, c.IncomeLevel,
+    ei.ECIID, ei.Year, ei.FDINet, ei.FDIInflowsToGDP, ei.FDIInflows,
+    ei.FDIOutflowsPercent, ei.FDIOutflows, ei.GDP, ei.GDPGrowth,
+    ei.GrossNationalSpending, ei.InflationConsumerPrices,
+    ei.InflationRate, ei.InflationPercent, ei.GoodsNetTrade,
+    ei.GoodsAndServicesNetTrade, ei.TradePercent, ei.ServicesTradedPercent, 
+	ei.GovernmentSpendingToGDP, ei.GovernmentSpendingAnnualGrowth,
+    ei.GovernmentSpending, ei.GovernmentEducationSpendingToGDP,
+    ei.GovernmentEducationSpendingToTotalSpending, ei.GovernmentMilitarySpendingToGDP,
+    ei.GovernmentMilitarySpendingToTotalSpending, ei.GovernmentMilitarySpending,
+    eci.ElectricityAccess, eci.AgriculturalCarbonDioxide, eci.TransportCarbonDioxide,
+    eci.WasteCarbonDioxide, eci.ForestArea, eci.RenewableEnergyConsumption,
+    eci.TotalGreenhouseGas, pdi.LongTermExternalDebt, pdi.TotalPSD, pdi.LongTermPSD,
+    pdi.ShortTermPSD, pdi.InterestPaymentsPercent, pdi.ExternalDebt, pdi.TotalDebt,
+    si.FemaleLifeExpectancy, si.MaleLifeExpectancy, si.TotalLifeExpectancy,
+    si.AdultLiteracyRate, si.YouthLiteracyRate, si.BasicDrinkingWater,
+    si.SafelyManagedDrinkingWater, si.BasicHandwashing, si.PovertyRatio,
+    si.NationalPovertyRatio, si.SocialPovertyRatio, si.UnemploymentWithAdvancedEducation,
+    si.UnemploymentWithBasicEducation, si.UnemploymentWithIntermediateEducation,
+    si.TotalUnemployment, si.YouthUnemployment, sti.ExchangeRate
+FROM 
+    Countries c
+JOIN 
+    Economic_Indicators ei ON c.CountryID = ei.CountryID
+JOIN 
+    Environmental_Indicators eci ON c.CountryID = eci.CountryID
+JOIN 
+    Public_Debt_Indicators pdi ON c.CountryID = pdi.CountryID
+JOIN 
+    Social_Indicators si ON c.CountryID = si.CountryID
+JOIN 
+    Statistical_Indicators sti ON c.CountryID = sti.CountryID
+WHERE 
+    ei.Year = 2022 AND eci.Year = 2022 AND pdi.Year = 2022 AND si.Year = 2022 AND sti.Year = 2022
